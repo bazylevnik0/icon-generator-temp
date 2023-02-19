8bc4e4a7810b55701c205854cce17b4aee094a16
bazylevnik0 <bazylevnik0@gmail.com>
2023-02-19 17:14:16 +0300

    add

---
+++ test/icon-generator-temp-test/search/1/src/icon_generator_temp_test_search-window.c
@@ -0,0 +1,54 @@
+/* icon_generator_temp_test_search-window.c
+ *
+ * Copyright 2023 nik0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "icon_generator_temp_test_search-config.h"
+#include "icon_generator_temp_test_search-window.h"
+
+struct _IconGeneratorTempTestSearchWindow
+{
+  GtkApplicationWindow  parent_instance;
+
+  /* Template widgets */
+  
+  GtkSearchEntry      *search_entry;
+};
+
+G_DEFINE_TYPE (IconGeneratorTempTestSearchWindow, icon_generator_temp_test_search_window, GTK_TYPE_APPLICATION_WINDOW)
+
+static void
+search_text_changed (GtkEntry *entry)
+{
+  g_print("%s\n","changed");
+}
+
+static void
+icon_generator_temp_test_search_window_class_init (IconGeneratorTempTestSearchWindowClass *klass)
+{
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/icon/generator/temp/test/search/icon_generator_temp_test_search-window.ui");
+  gtk_widget_class_bind_template_child    (widget_class, IconGeneratorTempTestSearchWindow, search_entry);
+  gtk_widget_class_bind_template_callback (widget_class, search_text_changed);
+}
+
+
+static void
+icon_generator_temp_test_search_window_init (IconGeneratorTempTestSearchWindow *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
---
+++ test/icon-generator-temp-test/search/temp.md
@@ -0,0 +1,14 @@
+two elements:<br>
+search input and area with previews<br>
+
+in widget gallery<br>
+about search<br>
+exist https://docs.gtk.org/gtk4/class.SearchEntry.html<br>
+also it is possible attach to search entry to https://docs.gtk.org/gtk4/class.SearchBar.html<br>
+
+1 - just add search with handle changing input<br>
+
+about prewievs:<br>
+exist this https://docs.gtk.org/gtk3/iface.FileChooser.html<br>
+and this https://docs.gtk.org/gtk4/class.IconView.html<br>
+
